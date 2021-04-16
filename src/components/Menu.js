4cb2a51cb0cb403ee80edaee2cfebc1611f69c96
edith <edith.maulandi@makina-corpus.com>
2021-04-16 17:50:22 +0200

    Use fonctionnal components and 2 spaces

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,27 +1,22 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 
-const Menu = (props) => (
-    <nav id="menu">
-        <div className="inner">
-            <ul className="links">
-                <li><Link onClick={props.onToggleMenu} to="/">Home</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/landing">Landing</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/generic">Generic</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/elements">Elements</Link></li>
-            </ul>
-            <ul className="actions vertical">
-                <li><a href="#" className="button special fit">Get Started</a></li>
-                <li><a href="#" className="button fit">Log In</a></li>
-            </ul>
-        </div>
-        <a className="close" onClick={props.onToggleMenu} href="javascript:;">Close</a>
-    </nav>
+const Menu = ({ onToggleMenu }) => (
+  <nav id="menu">
+    <div className="inner">
+      <ul className="links">
+        <li><Link onClick={onToggleMenu} to="/">Home</Link></li>
+        <li><Link onClick={onToggleMenu} to="/landing">Landing</Link></li>
+        <li><Link onClick={onToggleMenu} to="/generic">Generic</Link></li>
+        <li><Link onClick={onToggleMenu} to="/elements">Elements</Link></li>
+      </ul>
+      <ul className="actions vertical">
+        <li><a href="#" className="button special fit">Get Started</a></li>
+        <li><a href="#" className="button fit">Log In</a></li>
+      </ul>
+    </div>
+    <a className="close" onClick={onToggleMenu} href="javascript:;">Close</a>
+  </nav>
 )
-
-Menu.propTypes = {
-    onToggleMenu: PropTypes.func
-}
 
 export default Menu
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useState, useEffect } from 'react'
 
 import '../assets/scss/main.scss'
 import Header from './Header'
@@ -7,49 +6,30 @@ import Menu from './Menu'
 import Contact from './Contact'
 import Footer from './Footer'
 
-class Layout extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isMenuVisible: false,
-            loading: 'is-loading'
-        }
-        this.handleToggleMenu = this.handleToggleMenu.bind(this)
-    }
+const Layout = ({ children }) => {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const [loading, setLoading] = useState('is-loading');
 
-    componentDidMount () {
-        this.timeoutId = setTimeout(() => {
-            this.setState({loading: ''});
-        }, 100);
-    }
+  useEffect(() => { 
+    setTimeout(() => {
+      setLoading('');
+    }, 100);
+  });
 
-    componentWillUnmount () {
-        if (this.timeoutId) {
-            clearTimeout(this.timeoutId);
-        }
-    }
+  const handleToggleMenu = () => {
+      setIsMenuVisible(!isMenuVisible);
+  }
 
-    handleToggleMenu() {
-        this.setState({
-            isMenuVisible: !this.state.isMenuVisible
-        })
-    }
-
-    render() {
-        const { children } = this.props
-
-        return (
-            <div className={`body ${this.state.loading} ${this.state.isMenuVisible ? 'is-menu-visible' : ''}`}>
-                <div id="wrapper">
-                    <Header onToggleMenu={this.handleToggleMenu} />
-                    {children}
-                    <Contact />
-                    <Footer />
-                </div>
-                <Menu onToggleMenu={this.handleToggleMenu} />
-            </div>
-        )
-    }
+  return (
+      <div className={`body ${loading} ${isMenuVisible ? 'is-menu-visible' : ''}`}>
+          <div id="wrapper">
+              <Header onToggleMenu={handleToggleMenu} />
+              {children}
+              <Footer />
+          </div>
+          <Menu onToggleMenu={handleToggleMenu} />
+      </div>
+  )
 }
 
 export default Layout
