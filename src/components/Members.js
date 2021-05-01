af2640c15d49ecc42dcfe6846fefaee3980e334e
edith <edith.maulandi@makina-corpus.com>
2021-05-01 15:01:12 +0200

    ImproveMemebers section in Association page

--- src/components/Members.js
+++ src/components/Members.js
@@ -1,32 +1,69 @@
 import React from 'react';
+import { GatsbyImage } from "gatsby-plugin-image";
+
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Box } from '@material-ui/core';
 
 import useMembers from '../hooks/useMembers';
+import usePics from '../hooks/usePics';
+
+const useStyles = makeStyles(theme => ({
+  image: {
+    height: theme.spacing(25),
+  },
+  linkicon: {
+    margin: theme.spacing(1,0),
+  }
+}));
 
 const Members = () => {
+  const classes = useStyles();
+
   const members = useMembers();
+  const teamPics = usePics().filter(({ relativeDirectory }) => relativeDirectory === 'team-pics');
+  const getPic = (myName) => teamPics.find(({ name }) => myName === name)?.gatsbyImageData;
   
   return (
-    <div className="grid-wrapper">
-      {members.map(({ nom, nom_site, lien_site, linkedin, twitter }) => (
-        <div className="col-4">
-          <h5>{nom}</h5>
-          { lien_site 
-            ? (<a href={lien_site} target="_blank" className="button small">{nom_site}</a>)
-            : (<span>{nom_site}</span>)
-          }
-          <section>
-            <ul className="icons">
-              { twitter && (
-                <li><a href={twitter} target="_blank" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
-              )}
-              { linkedin && (
-                <li><a href={linkedin} target="_blank" className="icon alt fa-linkedin"><span className="label">Linkedin</span></a></li>
-              )}
-            </ul>
-          </section>
-        </div>
-      ))}
-    </div>
+    <Grid container>
+      {members.map(({ nom, pic_name, nom_site, lien_site, linkedin, twitter }) => {
+        const memberPic = getPic(pic_name);
+
+        return (
+          <React.Fragment key={nom}> 
+            <Grid item xs={12} sm={4} >
+              <Grid container 
+                direction="column"
+                justify="center"
+                alignItems="center"
+              >
+                <Grid item>
+                  <h5>{nom}</h5>
+                </Grid>
+                <Grid item>
+                  { memberPic && ( <GatsbyImage className={classes.image} image={memberPic} alt={pic_name}/>) }
+                </Grid>
+                <Grid item>
+                  { lien_site 
+                    ? (<a href={lien_site} target="_blank" className="button small">{nom_site}</a>)
+                    : (<span>{nom_site}</span>)
+                  }
+                </Grid>
+                <Box className={classes.linkicon}>
+                  <ul className="icons">
+                    { twitter && (
+                      <li><a href={twitter} target="_blank" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
+                    )}
+                    { linkedin && (
+                      <li><a href={linkedin} target="_blank" className="icon alt fa-linkedin"><span className="label">Linkedin</span></a></li>
+                    )}
+                  </ul>
+                </Box>
+              </Grid>
+            </Grid>
+          </React.Fragment>
+        )
+      })}
+    </Grid>
   );
 }
 
--- src/hooks/useMembers.js
+++ src/hooks/useMembers.js
@@ -6,6 +6,7 @@ export const useMembers = () => {
       allMembresCsv {
         nodes {
           nom
+          pic_name
           twitter
           nom_site
           linkedin
